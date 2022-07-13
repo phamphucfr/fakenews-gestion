540c3a192848e343d9b9cc61293694041b6a3c76
Phuc-PHAM-NGOC <phuc.phamng@gmail.com>
2022-07-13 11:20:16 +0200

    Update some private files

--- tools/MaConnexion.php
+++ tools/MaConnexion.php
@@ -2,9 +2,9 @@
 
 class MaConnexion {
     // Attributs
-    private static $url = "mysql:host=127.0.0.1:3306;dbname=phamphucfr;charset=utf8";
-    private static $user = "root";
-    private static $pass = "";
+    private static $url = "mysql:host=hostnameserveripadress:portnumber;dbname=nameofmydatabase;charset=utf8";
+    private static $user = "username";
+    private static $pass = "password";
     
     static function connect(){
         return new PDO(MaConnexion::$url, MaConnexion::$user, MaConnexion::$pass);
